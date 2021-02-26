96111c21692664fbc4add5bc8a8b2a863a64ca34
james <jayrh3246@gmail.com>
2021-02-25 20:29:54 -0500

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,22 @@
+require 'pry'
+
 class Animal
+        attr_reader :weight
+        attr_accessor :species, :nickname
+        
+        @@animal_info = []
+
+    def initialize(species, weight, nickname)
+        @species = species
+        @weight = weight
+        @nickname = nickname
+        @@animal_info << self
+    end
+
+    def self.all
+        @@animal_info 
+    end
 
 end
+
+chinchilla = Animal.new("rodent","1","Paco")
--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,21 @@
 class Zoo
+        attr_accessor :name, :location, #:animals
 
+    @@all = []
+
+    def initialize(name, location)
+        @name = name
+        @location = location
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def animals
+        # @animals
+        self.Animals.animals
+    end
 end
+
